2ac86d74a5dcbba7ae72afb6c8a02307a1a4e218
Ergün Karakuş <35894461+ekarakus@users.noreply.github.com>
2019-05-27 11:22:02 +0300

    Add files via upload

---
+++ WebApplication3/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(byte mezuniyet, int? maas)
+        {
+            if (maas.HasValue == false)
+            {
+                ViewData["hata"] = "Maaş boş bırakılamaz";
+            }
+            else
+            {
+                int zam = maas.Value * mezuniyet/100;
+                ViewData["zam"] = zam;
+                ViewData["yenimaas"] = zam + maas.Value;
+            }
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
